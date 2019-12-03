80d57c4edfa0edd0f90a02fd41d105486de18103
dusica8 <dusicaknezevic8@gmail.com>
2019-12-03 18:46:59 +0100

    First commit
    -Kreiran projekat u Qt Creator.

---
+++ Bolt/bolt.cpp
@@ -0,0 +1,15 @@
+#include "bolt.h"
+#include "ui_bolt.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
